afa974796a95f5b6e4a01d7ce23e6da90547619f
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-06-27 17:13:26 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int print_prg(va_list __attribute__((unused)), char *, unsigned int);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int print_str(va_list arguments, char *buf, unsigned int ibuf);
+
+#endif
